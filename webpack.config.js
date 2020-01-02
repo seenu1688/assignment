80a1a61f46e5815769b8f78253dd60e02ce6c71c
shasiseenu <45029949+shasiseenu@users.noreply.github.com>
2020-01-02 23:13:02 +0530

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,6 @@ module.exports = {
             test: /\.css$/,
             use: ['style-loader', 'css-loader'],
          },
-         {
-            test: /\.(png|j?g|svg|gif)?$/,
-            use: 'file-loader'
-         }
 ]
    },
    plugins: [
